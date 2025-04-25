efdd838bcd3529dbe28553eb047e17f2fa11d3aa
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-04-24 22:37:17 -0600

    Implement enemy death mechanics and adjust hitbox behavior

--- script/enemy.gd
+++ script/enemy.gd
@@ -6,9 +6,15 @@ var PLAYER = null
 var HEALTH = 100
 var PLAYER_IN_ATTACK_ZONE = false
 var CAN_TAKE_DAMAGE = true 
+var IS_DYING := false
+
+func _ready():
+	$AnimatedSprite2D.animation_finished.connect(_on_animated_sprite_2d_animation_finished)
 
 func _physics_process(_delta):
 	deal_with_damage()
+	if IS_DYING:
+		return
 	update_health()
 	
 	if PLAYER_CHASE:
@@ -24,7 +30,6 @@ func _physics_process(_delta):
 func _on_detection_area_body_entered(body: Node2D) -> void:
 	PLAYER = body
 	PLAYER_CHASE = true
-
 
 func _on_detection_area_body_exited(body: Node2D) -> void:
 	PLAYER = null
@@ -43,15 +48,33 @@ func _on_enemy_hitbox_body_exited(body: Node2D) -> void:
 		PLAYER_IN_ATTACK_ZONE = false
 		
 func deal_with_damage():
-		if PLAYER_IN_ATTACK_ZONE and General.PLAYER_CURRENT_ATTACK == true:
-			if CAN_TAKE_DAMAGE == true:
-				HEALTH = HEALTH - 25
-				$take_damage_cooldown.start()
-				CAN_TAKE_DAMAGE = false
-				print("slime health = ", HEALTH)
-				if HEALTH <= 0:
-					self.queue_free()
-				
+	if IS_DYING:
+		return
+
+	if PLAYER_IN_ATTACK_ZONE and General.PLAYER_CURRENT_ATTACK and CAN_TAKE_DAMAGE:
+		HEALTH -= 25
+		CAN_TAKE_DAMAGE = false
+		$take_damage_cooldown.start()
+		print("slime health =", HEALTH)
+
+		if HEALTH <= 0:
+			die()
+			await $AnimatedSprite2D.animation_finished
+			queue_free()
+
+func die():
+	IS_DYING = true
+	set_physics_process(false)
+	$detection_area/CollisionShape2D.disabled = true
+	$enemy_collision.disabled = true
+	$enemy_hitbox/enemy_hit_area.disabled = true
+	$health_bar.queue_free()
+	$AnimatedSprite2D.play("death")
+	
+func _on_animated_sprite_2d_animation_finished(anim_name) -> void:
+	if anim_name == "death":
+		queue_free()
+
 func _on_take_damage_cooldown_timeout() -> void:
 	CAN_TAKE_DAMAGE = true
 	
